f1a9009e2b460313cf1d23a2a221704ceecacebf
adutiny <113724666+adutiny@users.noreply.github.com>
2022-09-16 13:23:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -42,3 +42,22 @@ with left_column:
           st.empty()
 #st.write("jASJHS SJIQJ JQIJWI")
 #st.write("[Learn More](https://www.farouttinyhomes.com/)")
+
+[theme]
+
+#Primary accent for interactive elements
+primaryColor = '#7792E3'
+
+#Background color for the main content area
+backgroundColor = '#273346'
+
+#Background color for sidebar and most interactive widgets
+secondaryBackgroundColor = '#B9F1C0'
+
+#Color used for almost all text
+textColor = '#FFFFFF'
+
+ #Font family for all text in the app, except code blocks
+ #Accepted values (serif | sans serif | monospace)
+ #Default: "sans serif"
+font = "sans serif"
